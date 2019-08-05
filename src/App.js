e9d60fa182d8910516209ad737ebc05c9f70205e
Robson Vinicius <robsondgn@outlook.com>
2019-08-05 09:54:31 -0300

    Mudando para state o component authors

--- src/App.js
+++ src/App.js
@@ -3,39 +3,46 @@ import './App.css';
 import Table from './Table';
 
 class App extends Component {
-  authors = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco : '1000'
+  state = {
+    authors: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco : '1000'
 
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    }, 
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    }, 
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    },
-    {
-      nome: 'Karina',
-      livro: 'English in Brazil',
-      preco: '100'
-    }
-  ];
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      }, 
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      }, 
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Karina',
+        livro: 'English in Brazil',
+        preco: '100'
+      },
+      {
+        nome: 'Karina',
+        livro: 'English in Brazil',
+        preco: '100'
+      }
+    ],
+  };
 
   render() {
     return (
       <div className="App">
-        <Table authors = { this.authors } />
+        <Table authors = { this.state.authors } />
       </div>
     );
   }
