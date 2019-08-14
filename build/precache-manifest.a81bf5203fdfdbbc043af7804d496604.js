f198864ae7798529de1314119735acd0006c7b02
Robson Vinicius <robsondgn@outlook.com>
2019-08-14 13:03:01 -0300

    build

---
+++ build/precache-manifest.a81bf5203fdfdbbc043af7804d496604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd148cb88a34e28f9a94e5b909367538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb32207848c27a4e6f9d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "aaed09c5fb14dd8898b5",
+    "url": "/static/css/main.027df7d5.chunk.css"
+  },
+  {
+    "revision": "fb32207848c27a4e6f9d",
+    "url": "/static/js/2.30d58879.chunk.js"
+  },
+  {
+    "revision": "aaed09c5fb14dd8898b5",
+    "url": "/static/js/main.f2b58c0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
