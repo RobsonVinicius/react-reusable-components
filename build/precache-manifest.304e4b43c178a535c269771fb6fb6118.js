deecbf7f80f6ac5af0062cbb69faff9e2851d6a3
Robson Vinicius <robsondgn@outlook.com>
2019-09-02 21:23:53 -0300

    build com a regra de validacao

--- build/precache-manifest.304e4b43c178a535c269771fb6fb6118.js
+++ build/precache-manifest.304e4b43c178a535c269771fb6fb6118.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "746420dbc4efadb18ff5993193db90e6",
+    "revision": "6143516c975681d7a80a28e98c03313f",
     "url": "/index.html"
   },
   {
-    "revision": "c677fb61dc678a773b40",
+    "revision": "411a3c4b222975e7eed9",
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "3bb2cbd2cd1abc030993",
+    "revision": "70de59450ca45132da3f",
     "url": "/static/css/main.5cb13984.chunk.css"
   },
   {
-    "revision": "c677fb61dc678a773b40",
-    "url": "/static/js/2.421bcac8.chunk.js"
+    "revision": "411a3c4b222975e7eed9",
+    "url": "/static/js/2.e20def1c.chunk.js"
   },
   {
-    "revision": "3bb2cbd2cd1abc030993",
-    "url": "/static/js/main.2c0768a9.chunk.js"
+    "revision": "70de59450ca45132da3f",
+    "url": "/static/js/main.2e126c0e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cb46d6efbb9b017ffb1c27f67e09b55.js"
+  "/precache-manifest.304e4b43c178a535c269771fb6fb6118.js"
 );
 
 self.addEventListener('message', (event) => {
